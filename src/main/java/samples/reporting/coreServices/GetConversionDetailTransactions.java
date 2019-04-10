f35eda9f00e2d6ea4d829734efba5bd5dfa52ea4
KarthikeyanKumar <karthikeyan.cse.skcet@gmail.com>
2019-04-10 16:14:31 +0530

    Correcting name

--- src/main/java/samples/reporting/coreServices/GetConversionDetailTransactions.java
+++ src/main/java/samples/reporting/coreServices/GetConversionDetailTransactions.java
@@ -1,27 +1,23 @@
 package samples.reporting.coreServices;
 
 import java.util.Properties;
-
 import org.joda.time.DateTime;
-
 import com.cybersource.authsdk.core.MerchantConfig;
-
-import Api.NetFundingsApi;
+import Api.ConversionDetailsApi;
 import Data.Configuration;
 import Invokers.ApiClient;
 import Invokers.ApiException;
 /**
- * This is the method to get net funding information for merchant information
- * User should pass starttime, end tim.
+ * This is a sample code  to get conversion details for a given org Id
+ * User should pass org id, starttime and endtime
  *
  */
-public class GetNetFundingInformationForAMerchantAccount {
+public class GetConversionDetailTransactions {
 	private static String responseCode = null;
 	private static String status = null;
 	private static String organizationId="testrest";
-	private static DateTime startTime=new DateTime ("2018-10-01T00:00:00.0Z");
-	private static DateTime endTime=new DateTime ("2018-10-30T23:59:59.0Z");
-	private static String groupName="groupName"; 
+	private static DateTime startTime=new DateTime ("2019-03-21T00:00:00.0Z");
+	private static DateTime endTime=new DateTime ("2019-03-21T23:00:00.0Z");
 	private static Properties merchantProp;
 
 	public static void main(String args[]) throws Exception {
@@ -35,8 +31,9 @@ public class GetNetFundingInformationForAMerchantAccount {
 			merchantProp = Configuration.getMerchantDetails();
 			MerchantConfig merchantConfig = new MerchantConfig(merchantProp);
 			ApiClient.merchantConfig = merchantConfig;	
-			NetFundingsApi netFundingsApi=new NetFundingsApi(); 
-			netFundingsApi.getNetFundingInfo(startTime, endTime, organizationId, groupName);
+			
+			ConversionDetailsApi conversionDetailsApi=new ConversionDetailsApi();
+			conversionDetailsApi.getConversionDetail(startTime, endTime, organizationId);
 			responseCode = ApiClient.responseCode;
 			status = ApiClient.status;
 			System.out.println("ResponseCode :" + responseCode);
--- src/main/java/samples/reporting/coreServices/GetNetfundingInformationForAccountOrMerchant.java
+++ src/main/java/samples/reporting/coreServices/GetNetfundingInformationForAccountOrMerchant.java
@@ -1,23 +1,27 @@
 package samples.reporting.coreServices;
 
 import java.util.Properties;
+
 import org.joda.time.DateTime;
+
 import com.cybersource.authsdk.core.MerchantConfig;
-import Api.ConversionDetailsApi;
+
+import Api.NetFundingsApi;
 import Data.Configuration;
 import Invokers.ApiClient;
 import Invokers.ApiException;
 /**
- * This is a sample code  to get conversion details for a given org Id
- * User should pass org id, starttime and endtime
+ * This is the method to get net funding information for merchant information
+ * User should pass starttime, end tim.
  *
  */
-public class GetConversionDetails {
+public class GetNetfundingInformationForAccountOrMerchant {
 	private static String responseCode = null;
 	private static String status = null;
 	private static String organizationId="testrest";
-	private static DateTime startTime=new DateTime ("2019-03-21T00:00:00.0Z");
-	private static DateTime endTime=new DateTime ("2019-03-21T23:00:00.0Z");
+	private static DateTime startTime=new DateTime ("2018-10-01T00:00:00.0Z");
+	private static DateTime endTime=new DateTime ("2018-10-30T23:59:59.0Z");
+	private static String groupName="groupName"; 
 	private static Properties merchantProp;
 
 	public static void main(String args[]) throws Exception {
@@ -31,9 +35,8 @@ public class GetConversionDetails {
 			merchantProp = Configuration.getMerchantDetails();
 			MerchantConfig merchantConfig = new MerchantConfig(merchantProp);
 			ApiClient.merchantConfig = merchantConfig;	
-			
-			ConversionDetailsApi conversionDetailsApi=new ConversionDetailsApi();
-			conversionDetailsApi.getConversionDetail(startTime, endTime, organizationId);
+			NetFundingsApi netFundingsApi=new NetFundingsApi(); 
+			netFundingsApi.getNetFundingInfo(startTime, endTime, organizationId, groupName);
 			responseCode = ApiClient.responseCode;
 			status = ApiClient.status;
 			System.out.println("ResponseCode :" + responseCode);
