854bf0e15d2d2ffd03c56eb8bb2b950bd1b3a811
Gaurav Londhe <glondhe@visa.com>
2019-04-16 00:09:23 -0700

    Fixed sample for Authorization with TMS Payment instrument

--- src/main/java/samples/tms/paymentsWithToken/PaymentsInstrumentsForAuthorization.java
+++ src/main/java/samples/tms/paymentsWithToken/PaymentsInstrumentsForAuthorization.java
@@ -1,31 +1,29 @@
 package samples.tms.paymentsWithToken;
 
-import java.util.Properties;
-
-import com.cybersource.authsdk.core.MerchantConfig;
-
 import Api.PaymentInstrumentsApi;
+import Api.PaymentsApi;
 import Data.Configuration;
 import Invokers.ApiClient;
 import Invokers.ApiException;
-import Model.Body2;
-import Model.TmsV1PaymentinstrumentsPost201Response;
-import Model.Tmsv1instrumentidentifiersCard;
-import Model.Tmsv1paymentinstrumentsBillTo;
-import Model.Tmsv1paymentinstrumentsCard;
+import Model.*;
 import Model.Tmsv1paymentinstrumentsCard.TypeEnum;
-import Model.Tmsv1paymentinstrumentsInstrumentIdentifier;
+import com.cybersource.authsdk.core.MerchantConfig;
+
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 public class PaymentsInstrumentsForAuthorization {
 	private static String responseCode = null;
 	private static String status = null;
 	static TmsV1PaymentinstrumentsPost201Response response;
+	static PtsV2PaymentsPost201Response paymentResponse;
 	private static String profileId = "93B32398-AD51-4CC2-A682-EA3E93614EB1";
 	private static Properties merchantProp;
 
 	static Body2 body;
+	private static CreatePaymentRequest request;
 
-	private static Body2 getRequest() {
+	private static Body2 getPaymentInstrumentRequest() {
 		body = new Body2();
 		
 		Tmsv1paymentinstrumentsCard card=new Tmsv1paymentinstrumentsCard();
@@ -63,10 +61,37 @@ public class PaymentsInstrumentsForAuthorization {
 		process();
 	}
 
+	private static CreatePaymentRequest getpaymentRequest(String token) {
+		request = new CreatePaymentRequest();
+
+		Ptsv2paymentsClientReferenceInformation client = new Ptsv2paymentsClientReferenceInformation();
+		client.code("test_payment");
+		request.clientReferenceInformation(client);
+
+		Ptsv2paymentsOrderInformationAmountDetails amountDetails = new Ptsv2paymentsOrderInformationAmountDetails();
+		amountDetails.totalAmount("22");
+		amountDetails.currency("USD");
+
+		Ptsv2paymentsOrderInformation orderInformation = new Ptsv2paymentsOrderInformation();
+		orderInformation.amountDetails(amountDetails);
+		request.setOrderInformation(orderInformation);
+
+		Ptsv2paymentsPaymentInformationCustomer customer = new Ptsv2paymentsPaymentInformationCustomer();
+		customer.customerId(token);
+
+		Ptsv2paymentsPaymentInformation paymentInformation = new Ptsv2paymentsPaymentInformation();
+		paymentInformation.customer(customer);
+
+		request.setPaymentInformation(paymentInformation);
+
+		return request;
+
+
+	}
 	private static void process() throws Exception {
 
 		try {
-			body = getRequest();
+			body = getPaymentInstrumentRequest();
 
 			/* Read Merchant details. */
 			merchantProp = Configuration.getMerchantDetails();
@@ -76,16 +101,28 @@ public class PaymentsInstrumentsForAuthorization {
 			PaymentInstrumentsApi paymentInstrumentApi = new PaymentInstrumentsApi();
 			response = paymentInstrumentApi.tmsV1PaymentinstrumentsPost(profileId, body);
 
+			responseCode = ApiClient.responseCode;
+			status = ApiClient.status;
+			System.out.println("Create Payment Instrument");
+			System.out.println("ResponseCode :" +responseCode);
+			System.out.println("Status :" +status);
+			System.out.println("Payment Instrument Token :" + response.getId());
+			String token = response.getId();
+
+			request = getpaymentRequest(token);
+			TimeUnit.SECONDS.sleep(10);
+			PaymentsApi paymentApi = new PaymentsApi();
+			paymentResponse = paymentApi.createPayment(request);
 
 			responseCode = ApiClient.responseCode;
 			status = ApiClient.status;
-			
+
+			System.out.println("\nProcess a Payment");
 			System.out.println("ResponseCode :" +responseCode);
 			System.out.println("Status :" +status);
-			System.out.println(response.getId());
+			System.out.println("Payment ID :" +paymentResponse.getId());
 
 		} catch (ApiException e) {
-
 			e.printStackTrace();
 		}
 	}
