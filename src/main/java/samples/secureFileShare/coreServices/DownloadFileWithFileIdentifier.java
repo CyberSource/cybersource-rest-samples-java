42d5b035bb9aad8594f08cea079efc66fafa449a
Gabriel Broadwin Nongsiej <gnongsie@visa.com>
2019-10-30 15:18:45 +0530

    Changes to file download functionality

--- src/main/java/samples/secureFileShare/coreServices/DownloadFileWithFileIdentifier.java
+++ src/main/java/samples/secureFileShare/coreServices/DownloadFileWithFileIdentifier.java
@@ -1,15 +1,10 @@
 package samples.secureFileShare.coreServices;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.util.Properties;
+
+import org.apache.commons.io.FileUtils;
 
 import com.cybersource.authsdk.core.MerchantConfig;
 
@@ -17,13 +12,12 @@ import Api.SecureFileShareApi;
 import Data.Configuration;
 import Invokers.ApiClient;
 import Invokers.ApiException;
+import Invokers.ApiResponse;
 
 public class DownloadFileWithFileIdentifier {
 
 	private static String responseCode = null;
 	private static String status = null;
-	private static String responseBody = null;
-	
 	private static String organizationId = "testrest";
 	private static Properties merchantProp;
 	private  static String resourceFile = "SecureFile";
@@ -48,38 +42,28 @@ public class DownloadFileWithFileIdentifier {
 			apiClient.merchantConfig = merchantConfig;
 
 			SecureFileShareApi secureFileShareApi = new SecureFileShareApi(apiClient);
-			secureFileShareApi.getFileWithHttpInfo(fileId, organizationId);
+			ApiResponse<InputStream> responseStream = secureFileShareApi.getFileWithHttpInfo(fileId, organizationId);
 			
-			responseBody = apiClient.responseBody;
-			InputStream stream = new ByteArrayInputStream(responseBody.getBytes(StandardCharsets.UTF_8));
+			String contentType = responseStream.getHeaders().get("Content-Type").get(0);
 			
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-	        org.apache.commons.io.IOUtils.copy(stream, baos);
-	        byte[] bytes = baos.toByteArray();
-	        BufferedReader br = new BufferedReader(new InputStreamReader(
-	                (new ByteArrayInputStream(bytes))));
-
-	        String output;
-	        String reportType="csv";
-	        while ((output = br.readLine()) != null) {
-	            if(output.contains("xml")){
-	                reportType = "xml";
-	            }
-	        }
-	         BufferedReader br_write = new BufferedReader(new InputStreamReader(
-	                (new ByteArrayInputStream(bytes))));
-	        BufferedWriter bw = new BufferedWriter(new FileWriter(new File(FILE_PATH+resourceFile+"."+reportType)));
-	        while ((output = br_write.readLine()) != null) {
-	           bw.write(output+"\n");
-	        }
-	        bw.close();
+			String fileExtension = "csv";
+			
+			if (contentType.contains("json")) {
+				fileExtension = contentType.substring(contentType.length() - 4);
+			} else {
+				fileExtension = contentType.substring(contentType.length() - 3);
+			}
+			
+			File targetFile = new File(FILE_PATH + resourceFile + "." + fileExtension );
+			
+			FileUtils.copyInputStreamToFile(responseStream.getData(), targetFile);
 
 			responseCode = apiClient.responseCode;
 			status = apiClient.status;
 			System.out.println("ResponseCode :" + responseCode);
 			System.out.println("ResponseMessage :" + status);
 			System.out.println("File downloaded at the below location :");
-			System.out.println(new File(FILE_PATH + resourceFile + "." + reportType).getAbsolutePath());
+			System.out.println(new File(FILE_PATH + resourceFile + "." + fileExtension).getAbsolutePath());
 
 		} catch (ApiException e) {
 
--- src/main/java/samples/transactionBatches/coreServices/GetTransactionDetailsForGivenBatchId.java
+++ src/main/java/samples/transactionBatches/coreServices/GetTransactionDetailsForGivenBatchId.java
@@ -1,26 +1,22 @@
 package samples.transactionBatches.coreServices;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.util.*;
+
+import org.apache.commons.io.FileUtils;
+
 import com.cybersource.authsdk.core.MerchantConfig;
 
 import Api.*;
 import Data.Configuration;
 import Invokers.ApiClient;
+import Invokers.ApiResponse;
 
 public class GetTransactionDetailsForGivenBatchId{
 	private static String responseCode = null;
 	private static String status = null;
 	private static Properties merchantProp;
-	private static String responseBody = null;
 	public static String resourceFile = "BatchDetailsReport";
 	private static final String FILE_PATH = "src/main/resources/";
 	
@@ -39,37 +35,28 @@ public class GetTransactionDetailsForGivenBatchId{
 			apiClient.merchantConfig = merchantConfig;
 
 			TransactionBatchesApi apiInstance = new TransactionBatchesApi(apiClient);
-			apiInstance.getTransactionBatchDetails( id );
-
-			responseBody = apiClient.responseBody;
-			InputStream stream = new ByteArrayInputStream(responseBody.getBytes(StandardCharsets.UTF_8));
-
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			org.apache.commons.io.IOUtils.copy(stream, baos);
-			byte[] bytes = baos.toByteArray();
-			BufferedReader br = new BufferedReader(new InputStreamReader((new ByteArrayInputStream(bytes))));
-
-			String output;
-			String reportType = "csv";
-			while ((output = br.readLine()) != null) {
-				if (output.contains("xml")) {
-					reportType = "xml";
-				}
+			ApiResponse<InputStream> responseStream = apiInstance.getTransactionBatchDetailsWithHttpInfo( id );
+			
+			String contentType = responseStream.getHeaders().get("Content-Type").get(0);
+			
+			String fileExtension = "csv";
+			
+			if (contentType.contains("json")) {
+				fileExtension = contentType.substring(contentType.length() - 4);
+			} else {
+				fileExtension = contentType.substring(contentType.length() - 3);
 			}
-			BufferedReader br_write = new BufferedReader(new InputStreamReader((new ByteArrayInputStream(bytes))));
-			BufferedWriter bw = new BufferedWriter(
-					new FileWriter(new File(FILE_PATH + resourceFile + "." + reportType)));
-			while ((output = br_write.readLine()) != null) {
-				bw.write(output + "\n");
-			}
-			bw.close();
+			
+			File targetFile = new File(FILE_PATH + resourceFile + "." + fileExtension);
+			
+			FileUtils.copyInputStreamToFile(responseStream.getData(), targetFile);
 
 			responseCode = apiClient.responseCode;
 			status = apiClient.status;
 			System.out.println("ResponseCode :" + responseCode);
 			System.out.println("ResponseMessage :" + status);
-			System.out.println("Batch details downloaded at the below location :");
-			System.out.println(new File(FILE_PATH + resourceFile + "." + reportType).getAbsolutePath());
+			System.out.println("File downloaded at the below location :");
+			System.out.println(new File(FILE_PATH + resourceFile + "." + fileExtension).getAbsolutePath());
 		}
 		catch (Exception e) {
 			e.printStackTrace();
