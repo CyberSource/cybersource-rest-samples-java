6c83f707f02c93b373d788b2f1d2a97e20c52ae2
Gabriel Broadwin Nongsiej <gnongsie@visa.com>
2020-06-04 12:26:12 +0530

    Removing Flex folder-restructure. Adding Token Management folder-restructure for testing.

---
+++ src/main/java/samples/TokenManagement/InstrumentIdentifier/CreateInstrumentIdentifierCard.java
@@ -0,0 +1,57 @@
+package samples.TokenManagement.InstrumentIdentifier;
+
+import java.*;
+import java.util.*;
+import java.math.BigDecimal;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.LocalDate;
+
+import com.google.common.base.Strings;
+import com.cybersource.authsdk.core.MerchantConfig;
+
+import Api.*;
+import Data.Configuration;
+import Invokers.ApiClient;
+import Invokers.ApiException;
+import Model.*;
+
+public class CreateInstrumentIdentifierCard {
+	private static String responseCode = null;
+	private static String status = null;
+	private static Properties merchantProp;
+
+	public static void main(String args[]) throws Exception {
+		run();
+	}
+
+	public static TmsV1InstrumentIdentifiersPost200Response run() {
+		String profileid = "93B32398-AD51-4CC2-A682-EA3E93614EB1";
+		CreateInstrumentIdentifierRequest requestObj = new CreateInstrumentIdentifierRequest();
+
+		Tmsv1instrumentidentifiersCard card = new Tmsv1instrumentidentifiersCard();
+		card.number("411111111111111");
+		requestObj.card(card);
+
+		TmsV1InstrumentIdentifiersPost200Response result = null;
+		try {
+			merchantProp = Configuration.getMerchantDetails();
+			ApiClient apiClient = new ApiClient();
+			MerchantConfig merchantConfig = new MerchantConfig(merchantProp);
+			apiClient.merchantConfig = merchantConfig;
+
+			InstrumentIdentifierApi apiInstance = new InstrumentIdentifierApi(apiClient);
+			result = apiInstance.createInstrumentIdentifier(profileid, requestObj);
+
+			responseCode = apiClient.responseCode;
+			status = apiClient.status;
+			System.out.println("ResponseCode :" + responseCode);
+			System.out.println("ResponseMessage :" + status);
+			System.out.println(result);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	return result;
+	}
+}
