15c3f2cb5ee8dd0507c4a16bfc886d1cd032ff17
snavinch <snavinch@visa.com>
2021-01-05 18:14:27 +0530

    - Removing merchant config info

--- src/main/java/samples/authentication/AuthSampleCode/StandaloneOAuth.java
+++ src/main/java/samples/authentication/AuthSampleCode/StandaloneOAuth.java
@@ -14,9 +14,9 @@ public class StandaloneOAuth {
     private static String responseCode = null;
     private static String status = null;
     private static String code = "";
-    private static String clientId = "Byox4jxUi6";
+    private static String clientId = "";
     private static String grantType = "";
-    private static String clientSecret = "5ce33002-1653-45db-821b-d726a2c41c8f";
+    private static String clientSecret = "";
     private static String refreshToken = "";
     private static String accessToken = "";
     private static Properties merchantProp;
@@ -30,14 +30,14 @@ public class StandaloneOAuth {
         if(createUsingAuthCode)
         {
             // Create Access Token using Auth Code
-            code = "fcugsa";
+            code = "";
             grantType = "authorization_code";
             result = postAccessTokenFromAuthCode();
         }
         else {
             // Create Access Token using Refresh Token
             grantType = "refresh_token";
-            refreshToken = "eyJraWQiOiIxMGM2MTYxNzg2MzE2ZWMzMGJjZmI5ZDcyZGU4MzFjOSIsImFsZyI6IlJTMjU2In0.eyJqdGkiOiJiNDVlYTU2OS1mMjYzLTRiOGQtYTNlNy1kOTlmYzJiNjY1M2EiLCJzY29wZXMiOlsicGF5bWVudHNfd2l0aF9zdGFuZGFsb25lX2NyZWRpdCIsInBheW1lbnRzX3dpdGhvdXRfc3RhbmRhbG9uZV9jcmVkaXQiLCJ0cmFuc2FjdGlvbnMiXSwiaWF0IjoxNjA5ODI2NjgyMjQzLCJhc3NvY2lhdGVkX2lkIjoiZWJjMl9jYXNfb2F1dGh0cDIiLCJjbGllbnRfaWQiOiJCeW94NGp4VWk2IiwibWVyY2hhbnRfaWQiOiJjZ2syX3B1c2hfdGVzdHMiLCJleHBpcmVzX2luIjoxNjQxMzYyNjgyMjQzLCJ0b2tlbl90eXBlIjoicmVmcmVzaF90b2tlbiIsImdyYW50X3R5cGUiOiJhdXRob3JpemF0aW9uX2NvZGUiLCJncmFudF90aW1lIjoiMjAyMTAxMDQyMjAzIn0.dGm4jshBU2JCfI6o3h5qCcpe0_LlJ-HhmSo8BdDe91lYYtjaKJuBOer_dlHZCjMphL7IjVgTjNrCOHTiGv6JIUJQ6vK_log_FBIOtLylnKn6MCSzfwKpxSz7trz_eINmGGICz_6UhIN9w_f5qu3aWuO40Gv1yrIzlUOTajpgZ7CeDJpm9crGUmu3lNgjq1RPGuWaXmqFfBvnpmo-VVsdaR5tK2c1Iw1UTJJYvcjCv4Tw_gHxPcy3cMagoDQeUPqUV2YI7maddWuLnzeFO-01xfbNXMPMzfw8P09cznbWKSHZZlSEyXFaOMCz3zRoy-WJwHiNNasqZmvMI5P-ZrftdA";
+            refreshToken = "";
             result = postAccessTokenFromRefreshToken();
         }
 
