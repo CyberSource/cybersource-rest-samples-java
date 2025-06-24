9a4b982f10f17a8e6b289caaa2290e637934805a
gaubansa <gaubansa@visa.com>
2025-06-24 18:08:51 +0530

    sample update

--- src/main/java/samples/AccountUpdater/RetrieveBatchStatus.java
+++ src/main/java/samples/AccountUpdater/RetrieveBatchStatus.java
@@ -5,6 +5,8 @@ import Data.Configuration;
 import Invokers.ApiClient;
 import Invokers.ApiException;
 import Model.InlineResponse2008;
+import Model.InlineResponse2009;
+
 import com.cybersource.authsdk.core.MerchantConfig;
 
 import java.lang.invoke.MethodHandles;
@@ -26,7 +28,7 @@ public class RetrieveBatchStatus {
 
 	public static void run() {
 		String batchId = "16188390061150001062041064";
-		InlineResponse2008 result = null;
+		InlineResponse2009 result = null;
 		try {
 			merchantProp = Configuration.getMerchantDetails();
 			ApiClient apiClient = new ApiClient();
